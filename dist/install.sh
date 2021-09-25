868bc347ed5304160bb30127c3f557c6c9d17f3b
zanzarasultetto1 <91336438+zanzarasultetto1@users.noreply.github.com>
2021-09-25 10:54:24 +0200

    Update install.sh

--- dist/install.sh
+++ dist/install.sh
@@ -6,4 +6,8 @@ sleep 2
 wget "nonpersviluppatori.altervista.org/github/dist.zip"
 unzip dist.zip
 sleep 1
-./dist/PyWeb
+clear
+rm dist.zip
+echo "Installed. Your file is now in the dist folder."
+echo "Press ENTER to exit..."
+read 
